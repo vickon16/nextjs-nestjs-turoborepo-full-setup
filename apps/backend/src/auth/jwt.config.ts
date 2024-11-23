2c5746a948c4d5c528da2efa995b4bcfa6d71509
Nkachukwu Victor <nkachukwuvictor2016@gmail.com>
2024-11-23 08:51:56 +0100

    tracked new changes

---
+++ apps/backend/src/auth/jwt.config.ts
@@ -0,0 +1,10 @@
+import { registerAs } from '@nestjs/config';
+import { JwtSignOptions } from '@nestjs/jwt';
+
+export default registerAs(
+  'jwt',
+  (): JwtSignOptions => ({
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  }),
+);
